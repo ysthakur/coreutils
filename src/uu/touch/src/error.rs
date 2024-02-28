9ef368331ca3e2c931041ca01066fe0abf2d1414
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-02-28 00:28:42 -0500

    touch: spell-checker ignore 'uioerror'

--- src/uu/touch/src/error.rs
+++ src/uu/touch/src/error.rs
@@ -2,6 +2,9 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+
+// spell-checker:ignore (misc) uioerror
+
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result};
 use std::io::ErrorKind;
